8681b01cec9d218108da0fd4efdb4af486eebc49
fazlerabby1827052 <rabby1827052@stud.kuet.ac.bd>
2025-05-21 10:47:56 +0600

    Write some handler for testing seeking start time and end time

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
-  return <>sajid</>;
+  return <>
+    <VideoPlayer/>
+  </>;
 }
 
 export default App;
