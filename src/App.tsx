0422ad7e6d820999a52ed12410a6634ac6d18284
sazid62 <sajidurrahman3s2@gmail.com>
2025-05-21 13:13:16 +0600

    merged from main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
-import Heatmap from "./components/Heatmap";
+
 import Video from "./components/Video";
-import VideoPlayer from "./components/Video";
 
 function App() {
-  const pv = [1, 2];
-
   return (
     <>
       <Video video_id={12} />
+      <Video video_id={15} />
     </>
   );
 }
